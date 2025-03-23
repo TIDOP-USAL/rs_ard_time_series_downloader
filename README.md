107356cc0884ce43e04d0d2a814143b1fab9bd83
DHL <david.hernandez@uclm.es>
2025-03-23 11:53:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# **FOREST FIRE SEVERITY ESTIMATION - QGIS**
+
+## A plugin to estimate forest fire severity using DNBR index from Sentinel-2 and Earth Engine
+
+[https://github.com/UCLM-PAFyC/forest_fire_severity_estimation](https://github.com/UCLM-PAFyC/forest_fire_severity_estimation)
+
+## **David Hernández López**
+
+Professor of Geomatics
+
+University of Castilla-La Mancha - UCLM, 
+david.hernandez@uclm.es
+
+University of Salamanca - USAL, 
+dhernand@usal.es
+
+ORCID, [https://orcid.org/0000-0001-9874-5243](https://orcid.org/0000-0001-9874-5243)
+
+**Research groups:**
+
+UCLM - PAFyC, [http://pafyc.uclm.es](http://pafyc.uclm.es)
+
+USAL - TIDOP, [http://tidop.usal.es](http://tidop.usal.es)
+
+
+Beta version: 0.1, Minimum QGIS version: 3.4
+
+Any use of this version is allowed without 
+responsibility for the author
+
+Please report any bugs and enhancement ideas by email: david.hernandez@uclm.es
+
+Contact the author to request any custom development 
+
+Pre-requisites:
+
+- Install Google Earth Plugin from QGIS Python Plugins Repository
+- Create Google Earth Engine API KEY when plugin require, using your Google account
+
+Any use of this version is allowed without 
+responsibility for the author
+
+To do ...
+- Create a user manual
+- ...
+
---
+++ definitions.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+  QGIS plugin Remote Sensing ARD time series downloader using OPENEO
+        copyright            : (C) David Hernandez Lopez
+        email                : david.hernandez@uclm.es
+ ***************************************************************************/
+"""
+
+__author__ = 'david.hernandez@uclm.es'
+
+CONST_AUTHOR_MAIL = 'david.hernandez@uclm.es'
+CONST_SETTINGS_PLUGIN_NAME = "rs_ard_time_series_downloader"
+CONST_SETTINGS_FILE_NAME = "rs_ard_time_series_downloader.ini"
+CONST_PROGRAM_NAME = "Remote sensing ARD time series downloader"
+CONST_PROGRAM_TITLE = "Remote sensing ARD time series downloader"
+CONST_NO_COMBO_SELECT = " ... "
+CONST_TEMPLATE_PATH = "\\templates"
+CONST_DNBR_SYMBOLOGY_TEMPLATE = "\\BurnSeverityClassesUSGS_UN-SPIDER.qml"
+CONST_DATE_STRING_TEMPLATE = "yyyy-MM-dd"
+CONST_INITIAL_DATE_DEFAULT = "2023-10-01"
+CONST_FINAL_DATE_DEFAULT = "2024-09-30"
+CONST_SETTINGS_LAST_PATH_TAG = "last_path"
+CONST_SETTINGS_INITIAL_DATE_TAG = "inital_date"
+CONST_SETTINGS_FINAL_DATE_TAG = "final_date"
+CONST_SETTINGS_OUTPUT_PATH_TAG = "output_path"
+CONST_MINIMAL_DATES_INTERVAL = 15
+# CONST_EARTH_ENGINE_PLUGIN = "ee_plugin"
+CONST_OPENEO_PIP_INSTALL_PATH = '\\site-packages\\openeo'
+CONST_OPENEO_AUTH_PATH = '\\Scripts\\openeo-auth.exe'
+CONST_OPENEO_REFRESH_TOKENS = 'refresh-tokens.json'
+CONST_OPENEO_MAPCANVAS_FEATURE_ID = "map_canvas"
+CONST_OPENEO_EXTENT_CRS = "4258"
+openEO_providers = {}
+openEO_providers['Copernicus Data Space Ecosystem'] = 'openeo.dataspace.copernicus.eu'
+feature_field_id_candidates = []
+feature_field_id_candidates.append("id")
+feature_field_id_candidates.append("fid")
+
+
